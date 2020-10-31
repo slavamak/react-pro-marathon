b749e33e320f4cabfbfb7d2d5bcc43e178b6a8ca
slavamak <slavamak.dev@gmail.com>
2020-10-31 22:49:09 +0300

    Homework for Lesson-2

---
+++ src/example.ts
@@ -0,0 +1,33 @@
+interface ConcatFn {
+  (a: string, ...rest: string[]): string
+}
+
+const concat: ConcatFn = (first, ...rest) => {
+  return first + rest.join('')
+}
+
+concat('Hello', ', ', 'world!')
+
+
+interface MyHometaskInterface {
+  howIDoIt: string,
+  simeArray: Array<string | number>,
+  withData: Array<{ howIDoIt: string, simeArray: Array<string | number> }>
+}
+
+const MyHometask: MyHometaskInterface = {
+  howIDoIt: "I Do It Wel",
+  simeArray: ["string one", "string two", 42],
+  withData: [{ howIDoIt: "I Do It Wel", simeArray: ["string one", 23] }],
+}
+
+
+interface MyArray<T> {
+  [n: number]: T
+  reduce<U>(fn: (acc: T, curr: T, i?: number, arr?: T[]) => U, initial?: number) : U
+}
+
+const arr: MyArray<number> = [1, 2, 767]
+
+arr.reduce((accumulator, currentValue) => accumulator + currentValue, 5)
+arr.reduce((accumulator, currentValue) => accumulator + currentValue + 'yo')
